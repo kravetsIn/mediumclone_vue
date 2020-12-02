7a0b783483d7495229163a5be053455820b12c4c
Vladyslav Kravets <hello.kravets@gmail.com>
2020-12-02 16:53:32 +0200

    added set/get localStorage wrapper. save accesstoken after reg

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,5 +1,6 @@
 import mutations from '@/store/mutations';
 import authApi from '@/api/auth';
+import { setItem } from '@/helpers/persistanceStorage';
 
 const { REGISTER_START, REGISTER_SUCCESS, REGISTER_FAILURE } = mutations;
 
@@ -37,6 +38,7 @@ const authStore = {
         authApi.register(credentials)
           .then((response) => {
             const { data: { user } } = response;
+            setItem('accessToken', user.token);
             commit(REGISTER_SUCCESS, user);
             resolve();
           })
