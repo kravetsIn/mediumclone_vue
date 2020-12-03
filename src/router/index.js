773ca40a3d15341bb3d9fc3c1d01b98351a614e2
Vladyslav Kravets <hello.kravets@gmail.com>
2020-12-03 23:07:40 +0200

    added all routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '@/views/Home.vue';
+import GlobalFeed from '@/views/GlobalFeed.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
+    name: 'globalFeed',
+    component: GlobalFeed,
   },
   {
     path: '/register',
@@ -19,6 +19,48 @@ const routes = [
     path: '/login',
     name: 'login',
     component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/feed',
+    name: 'yourFead',
+    component: GlobalFeed,
+  },
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/:slug',
+    name: 'article',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/:slug/edit',
+    name: 'editArticle',
+    component: GlobalFeed,
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: GlobalFeed,
+  },
+
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component: GlobalFeed,
+  },
+
+  {
+    path: '/profiles/:slug/favorites',
+    name: 'userProfileFavorites',
+    component: GlobalFeed,
   },
 ];
 
